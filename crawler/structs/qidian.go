4300f668aa258ca2961b67d95c8bec608b19f723
amd_device <wmsjshihao@gmail.com>
2024-04-09 19:46:49 +0800

    add new type

--- crawler/structs/qidian.go
+++ crawler/structs/qidian.go
@@ -21,5 +21,22 @@ type ListEntity_qidian struct {
 	Grade      float32 // 评分
 	GradersNum int     // 评分人数
 	Rank       int     // 排名
-	BookUrl    string	// 小说页面url
+	BookUrl    string  // 小说页面url
 }
+
+type rank_type string
+
+const (
+	yuepiao         rank_type = "yuepiao"
+	changxiao       rank_type = "hotsales"
+	yueduzhishu     rank_type = "readindex"
+	shuyou          rank_type = "newfans"
+	tuijian         rank_type = "recom"
+	shoucang        rank_type = "collect"
+	gengxin         rank_type = "vipup"
+	vipshoucang     rank_type = "vipcollect"
+	qianyuezuozhe   rank_type = "signnewbook"
+	gongzhongzuozhe rank_type = "pubnewbook"
+	xinrenqianyue   rank_type = "newsign"
+	xinrenzuozhe    rank_type = "newauthor"
+)
